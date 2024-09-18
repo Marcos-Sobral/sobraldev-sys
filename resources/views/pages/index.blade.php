a2c6822d72f3c3d44e3fa4b40bb43f60e66ba908
Marcos-Sobral <vini15oficial@gmail.com>
2024-09-18 09:19:44 -0300

    💥: S4: fix: adicionando a logo em formato favicon para pesquisa no google

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="icon" href="{{ URL::asset('assets/img/logo/3.png') }}"  type="image/x-icon">
+    <link rel="icon" href="{{ URL::asset('assets/img/logo/favicon.ico') }}"  type="image/x-icon">
     <title>Portifolio - Sobral Dev</title>
 </head>
 <body>
