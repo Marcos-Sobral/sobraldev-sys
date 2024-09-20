76c27d734b435aebaf085eabea9bb4d90255f14a
Marcos-Sobral <vini15oficial@gmail.com>
2024-09-20 15:59:58 -0300

    💥: S4: fix: Revertendo titulo para Marcos Sobral:

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="icon" href="{{ URL::asset('assets/img/logo/favicon.ico') }}"  type="image/x-icon">
-    <title>Portifolio - Sobral Dev</title>
+    <title>Marcos Sobral: sobraldev</title>
 </head>
 <body>
     <livewire:header />
