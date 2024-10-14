e56591692bdd49d72daaa5e8f33d248bb05869f8
Marcos-Sobral <vini15oficial@gmail.com>
2024-10-14 01:07:24 -0300

    💥: S4: fix: corrigindo bug do dropmenu de curriculo no header

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -1,42 +1,43 @@
 <header id="header">
-      <nav class="navbar navbar-expand-lg navbar-dark">
-        <div class="container-fluid d-flex justify-content-between align-items-center mx-5 my-3">
-          
-          <a class="navbar-brand d-flex align-items-center " href="{{URL::route('principal')}}">
-            <img src="{{ URL::asset('assets/img/logo/2.png') }}" alt="Logo" width="120" height="40" class="d-inline-block align-text-top me-2">
-          </a>
-          <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-</button>
+  <nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container-fluid d-flex justify-content-between align-items-center mx-5 my-3">
+      <a class="navbar-brand d-flex align-items-center" href="{{ URL::route('principal') }}">
+        <img src="{{ URL::asset('assets/img/logo/2.png') }}" alt="Logo" width="120" height="40" class="d-inline-block align-text-top me-2">
+      </a>
+      <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-          <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-            <ul id="nav_item" class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <a class="nav-link " href="#Meus_projetos">Ultimas atualizações</a>
-              </li>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul id="nav_item" class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#Meus_projetos">Últimas atualizações</a>
+          </li>
 
-              <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown"
-                      aria-expanded="false">
-                      Curriculos
-                  </a>
-                  <ul class="dropdown-menu dropdown-menu-end">
-                      <li><a class="dropdown-item fw-bold" target="_blank" href="http://lattes.cnpq.br/4305160667586368">Carreira Ciêntifica - Lattes</a></li>
-                      <li><a class="dropdown-item fw-bold" target="_blank" href="https://www.canva.com/design/DAGPnxq9kuY/LzeaPUUIKLk3i3fTnhna4Q/edit?utm_content=DAGPnxq9kuY&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">Desenvolvedor PHP</a></li>
-                      <li><a class="dropdown-item fw-bold" target="_blank" href="https://www.canva.com/design/DAGPn7tKC58/o7Nf0sOQJF48URmBvC0RGw/edit?utm_content=DAGPn7tKC58&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">Desenvolvedor Java</a></li>
-                      <li><a class="dropdown-item fw-bold" target="_blank" href="">Design UX/UI</a></li>
-                  </ul>
-              </li>
-
-              <li class="nav-item"><a href="https://github.com/Marcos-Sobral" class=" nav-link fs-6" target="_blank">GitHub <i class="fab  fa-github"></i></a></li>
-
-              <li class="nav-item"><a href="https://www.linkedin.com/in/marcossobral-ti" target="_blank" class=" nav-link fs-6">linkedin <i class="fab fa-linkedin"></i></a></li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Currículos
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end">
+              <li><a class="dropdown-item fw-bold" target="_blank" href="http://lattes.cnpq.br/4305160667586368">Carreira Científica - Lattes</a></li>
+              <li><a class="dropdown-item fw-bold" target="_blank" href="https://www.canva.com/design/DAGPnxq9kuY/LzeaPUUIKLk3i3fTnhna4Q/edit?utm_content=DAGPnxq9kuY&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">Desenvolvedor PHP</a></li>
+              <li><a class="dropdown-item fw-bold" target="_blank" href="https://www.canva.com/design/DAGPn7tKC58/o7Nf0sOQJF48URmBvC0RGw/edit?utm_content=DAGPn7tKC58&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">Desenvolvedor Java</a></li>
+              <li><a class="dropdown-item fw-bold" target="_blank" href="">Design UX/UI</a></li>
             </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
-    <script>
+          </li>
 
-    </script>
+          <li class="nav-item">
+            <a href="https://github.com/Marcos-Sobral" class="nav-link fs-6" target="_blank">GitHub <i class="fab fa-github"></i></a>
+          </li>
+
+          <li class="nav-item">
+            <a href="https://www.linkedin.com/in/marcossobral-ti" target="_blank" class="nav-link fs-6">LinkedIn <i class="fab fa-linkedin"></i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
+
+<!-- JavaScript do Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
