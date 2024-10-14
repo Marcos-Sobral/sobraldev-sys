e2cc7defbf4497f90849c5db222f166bfceae836
Marcos-Sobral <vini15oficial@gmail.com>
2024-10-14 00:56:16 -0300

    ✨: S4: feat: adicionando link do contato no sobremim

--- resources/views/livewire/sobremim.blade.php
+++ resources/views/livewire/sobremim.blade.php
@@ -2,9 +2,9 @@
 @foreach ($users as $user)
 <section class="main-section">
     <div class="text-section">
-        <h2 class="main-title">Marcos Sobral - Desenvolvedor <span>Web</span></h2>
+        <h2 class="main-title">{{ $user->name }} - Desenvolvedor <span>Web</span></h2>
         <p class="main-content">{{ $user->summary }}</p>
-        <button class="btn-grande button-purple">Entrar em contato</button>
+        <button class="btn-grande button-purple" onclick="window.location.href='mailto:marcossobraldev@gmail.com'">Entrar em contato</button>
     </div>
     <figure class="profile-image">
         <img src="{{ asset('images/' . $user->photo) }}" alt="Seu Nome">
