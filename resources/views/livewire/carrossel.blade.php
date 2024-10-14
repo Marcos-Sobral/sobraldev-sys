e3612e82f7246ad73eb78c5f59d4c3e167232a84
Marcos-Sobral <vini15oficial@gmail.com>
2024-10-14 00:40:41 -0300

    ♻️: S4: refactor: refatoração da estilização do carousel

--- resources/views/livewire/carrossel.blade.php
+++ resources/views/livewire/carrossel.blade.php
@@ -1,27 +1,51 @@
-<div id="carouselExampleCaptions" class="carousel slide mb-5">
-    <div class="carousel-indicators">
-        @foreach($carrosseis as $index => $carrossel)
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="{{ $index }}" class="{{ $index == 0 ? 'active' : '' }}" aria-current="true" aria-label="Slide {{ $index + 1 }}"></button>
-        @endforeach
+
+
+<div class="container-fluid mt-5">
+    <div class="text-center mb-5">
+        <h2 id="Meus_projetos" class="featurette-heading fw-bold lh-1 color-purple">Tecnologias que utilizo</h2>
     </div>
-    <div class="carousel-inner">
-        @foreach($carrosseis as $index => $carrossel)
-            <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
-                <img src="{{ URL::asset('images/' . $carrossel->carrossel_img) }}" class="d-block w-100" alt="...">
-                <div class="carousel-caption">
-                    @foreach($carrossel->carrosselLink as $link)
-                        <p><a class="btn btn-light" href="{{ $link->carrossel_link_url }}"  target="_blank">Ver mais</a></p>
+    <div class="row">
+        <div class="col-sm-12">
+            <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    @foreach($carrosseis->chunk(3) as $index => $chunk)
+                        <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
+                            <div class="container">
+                                <div class="row">
+                                    @foreach($chunk as $carrossel)
+                                        <div class="col-sm-12 col-lg-4 mb-3">
+                                            <div class="card" style="width: 100%; margin: auto;">
+                                                <img src="{{ URL::asset('images/' . $carrossel->carrossel_img) }}" class="card-img-top" alt="Imagem">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">{{ $carrossel->carrossel_titulo ?? 'Título Padrão' }}</h5>
+                                                    <p class="card-text">{{ $carrossel->carrossel_descricao ?? 'Descrição padrão do carrossel.' }}</p>
+                                                    @if($carrossel->carrosselLink->isNotEmpty())
+                                                        @foreach($carrossel->carrosselLink as $link)
+                                                            <a href="{{ $link->carrossel_link_url }}" class="btn-medio button-purple" target="_blank">Ver mais</a>
+                                                        @endforeach
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
                     @endforeach
                 </div>
+
+                <a class="carousel-control-prev" href="#carouselExample" role="button" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExample" role="button" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </a>
             </div>
-        @endforeach
+        </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
